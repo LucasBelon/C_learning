fcc2f5512261e9df28d4cc8c2f473365cd6e970e
Lucas Belon <lucasgouveiabelon@gmail.com>
2021-12-04 00:46:14 -0300

    added til page 38 of the book

---
+++ NumberOne/nineth_program_v1.c
@@ -0,0 +1,39 @@
+/* This program is meant to write a function that makes
+ * the positive integer power of a number.*/
+
+main(){
+    int i ;
+    for (i = 0; i < 31 ; ++i){
+        printf("%d %d %d\n", i, pot(2, i), pot(-3, i) ) ;
+    }
+}
+
+pot(x, n)
+int x, n;
+{
+    int i, p;
+
+    p = 1;
+    for (i = 1; i <= n; ++i){
+        p = p * x;
+    }
+    return (p);
+}
+
+
+/* This program is on page 34 of the book
+ * Every function has the format:
+ *
+ * name (list of arguments)
+ * argument declaration
+ * {
+ *  Declarations
+ *  Commands
+ * }
+ * 
+ * Fuctions can show up at any order, and in one or two source-files.
+ * if the source is on two or more files you'll need to tell more things
+ * make it run
+ * I don't have a clue about how to make it run with bigger numbers. It
+ * goes to 10e10 algarisms*/
+
