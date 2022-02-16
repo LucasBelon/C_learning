d7581d49d4897d13e36ae330b38e4118e8a0960c
Lucas Belon <lucasgouveiabelon@gmail.com>
2022-02-16 05:19:54 -0300

    Added comments on code
    Since I'm always realising new things about programming, from times to
    times I like to add some comments to old files. It works as a way to
    keep my discoveries at some place

--- NumberOne/00-1gcc_compiled.c
+++ NumberOne/00-1gcc_compiled.c
@@ -17,10 +17,16 @@ int main()
  * Answer: To tell to the system that the function came to an end
  * why is stdio with a .h extension?
  * Answer: It stands for a header file, that says "oh, I'll call the function you need"
+ * What is that "int" at the main function? 
+ * Answer: Since we execute our programs they become processes and we need to return
+ * some signal to our system that says "Oh yeah, we're good at here. I'm finished". 
+ * And more, sometimes it doesn't return anything, so it becomes a void function 
+ * that returns nothing.
  * to compile this program it was used:
  * $ gcc 00-1testWithGcc.c -o testWithGcc 
  * 
  * In Termux:
  * We don't have the gcc compiler program, we need to use the Clang, that uses the
  * llvm. There are a front-end and back-end parts of a compiler. We need both
- * to operate with them, so we say that gcc is a compiler, but clang isn't.*/
+ * to operate with them, so we say that gcc is a compiler, but Clang isn't, 
+ * Clang is a front-end to the llvm that compiles our programs.*/
